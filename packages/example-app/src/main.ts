e9a787fe431a9cd68ebf1460f8a8e8adeaacf070
Martin Gysel <me@bearsh.org>
2026-04-01 11:04:08 +0200

    example-app: improve visual experience

--- packages/example-app/src/main.ts
+++ packages/example-app/src/main.ts
@@ -1,4 +1,18 @@
 import goMath from 'go:./math'
 
-const el = document.getElementById('app')
-if (el) el.innerHTML = `- Example app started<br>- go file imported<br>- call function <code>add(1, 2)</code><br>- result: <code>${goMath.add(1, 2)}</code>`
+import './demo.css';
+
+const el = document.getElementById('app');
+if (el) {
+  el.innerHTML = `
+    <div id="app-demo">
+      <h1>Vite + Go WASM Demo</h1>
+      <p>The Go function <code>add(1, 2)</code> was successfully imported and executed:</p>
+      <div class="result-box">
+        <span class="result-label">Result:</span>
+        <span class="result-value">${goMath.add(1, 2)}</span>
+      </div>
+      <p class="powered">powered by <b>vite-plugin-golang-wasm-lite</b></p>
+    </div>
+  `;
+}
