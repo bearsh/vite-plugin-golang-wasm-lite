818939e5d184c5dbfec3772b3cd273eab2bd9c03
Martin Gysel <me@bearsh.org>
2026-04-01 21:44:47 +0200

    example app: use latest math go pkg

--- packages/example-app/src/main.ts
+++ packages/example-app/src/main.ts
@@ -1,7 +1,7 @@
 // either import from a source directory
 import goMath from 'go:./math'
 // or from a remote module (with optional version)
-//import goMath from 'go:github.com/bearsh/vite-plugin-golang-wasm-lite/packages/example-app/src/math@175c22cbdbb665a0b0139360b09f295b6d8c9580'
+//import goMath from 'go:github.com/bearsh/vite-plugin-golang-wasm-lite/packages/example-app/src/math@latest'
 
 import './demo.css';
 
